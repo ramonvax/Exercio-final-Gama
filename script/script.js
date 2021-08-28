b9d87ee63ad1acc8a76f6417a5a6fde76562276b
Ramom <ramonvax@yahoo.com.br>
2021-08-27 22:27:53 -0300

    criaçao aviso cdpf valido e invalido e deito ajuste no css dos mesmo; vinculo do javaScript ao html; criado a funcao de validar

---
+++ script/script.js
@@ -0,0 +1,6 @@
+console.log("JavaScript carregado")
+function validate() {
+    console.log("Iniciando validacao")
+    var cpf = document.getElementById("cpfDigitado").value
+    console.log(cpf)
+}

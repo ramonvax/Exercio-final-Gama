e66ce8d9d1a5fa88926a9ce65771f49cd29a4881
Ramom <ramonvax@yahoo.com.br>
2021-08-27 23:14:39 -0300

    criado a validacao de 11 digitos no cpf

--- script/script.js
+++ script/script.js
@@ -1,6 +1,23 @@
 console.log("JavaScript carregado")
+function verificaSoma(cpf) {
+    if (cpf.length === 11) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 function validate() {
     console.log("Iniciando validacao")
-    var cpf = document.getElementById("cpfDigitado").value
-    console.log(cpf)
+    var cpf = document.getElementById("cpfDigitado").value;
+    document.getElementById("CPFValido").style.display = "none"
+    document.getElementById("CPFInvalido").style.display = "none"
+    var somaCpf = verificaSoma(cpf);
+    if (somaCpf) {
+        document.getElementById("CPFValido").style.display = "block"
+    }
+    else {
+        document.getElementById("CPFInvalido").style.display = "block"
+    }
 }
